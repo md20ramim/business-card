0f79bbfa6a38603e530eb99d24347e8b13f531ee
md19ramim <87000627+md19ramim@users.noreply.github.com>
2021-08-17 15:18:36 +0600

    seventh commit

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // submit-btn
+function inputText(letter, part){
+    const logoLetter = document.getElementById(letter).value;
+    const yourLogo = document.getElementById(part).innerText;
+    document.getElementById(part).innerText = logoLetter;
+    document.getElementById(letter).value = "";
+}
 const submitBtn =document.getElementById('submit');
 submitBtn.addEventListener('click', function(){
     const cardTemplates= document.getElementById("templates");
@@ -8,48 +14,13 @@ submitBtn.addEventListener('click', function(){
     const downloadBtn = document.getElementById("download-btn-id");
     downloadBtn.style.display ="block";
 
-    const logoLetter = document.getElementById("logo-letter").value;
-    const yourLogo = document.getElementById("logo-part").innerText;
-    document.getElementById("logo-part").innerText = logoLetter;
-    document.getElementById("logo-letter").value = "";
-
-    const nameLetter = document.getElementById("your-name").value;
-    const yourName = document.getElementById("name").innerText;
-    document.getElementById("name").innerText = nameLetter;
-    document.getElementById("your-name").value=""
-
-    const professionLetter = document.getElementById("your-profession").value;
-    const yourProfession = document.getElementById("profession").innerText;
-    document.getElementById("profession").innerText = professionLetter;
-    document.getElementById("your-profession").value ="";
-
-    const companyLetter = document.getElementById("company-letter").value;
-    const yourCompany = document.getElementById("company-name").innerText;
-    document.getElementById("company-name").innerText = companyLetter;
-    document.getElementById("company-letter").value ="";
-
-    const taglineLetter = document.getElementById("tagline-letter").value;
-    const yourTagline = document.getElementById("company-tagline").innerText;
-    document.getElementById("company-tagline").innerText = taglineLetter;
-    document.getElementById("tagline-letter").value = "";
-
-    const emailLetter = document.getElementById("your-email").value;
-    const yourEmail = document.getElementById("email").innerText;
-    document.getElementById("email").innerText = emailLetter;
-    document.getElementById("your-email").value = "";
-
-    const contuctLetter = document.getElementById("your-contuct").value;
-    const yourcontuct = document.getElementById("contuct").innerText;
-    document.getElementById("contuct").innerText = contuctLetter;
-    document.getElementById("your-contuct").value="";
-
-    const locationLetter = document.getElementById("location-letter").value;
-    const yourLocation = document.getElementById("your-location").innerText;
-    document.getElementById("your-location").innerText = locationLetter;
-    document.getElementById("location-letter").value= "";
-
-    const websiteLetter = document.getElementById("website-letter").value;
-    const yourWebsite = document.getElementById("your-website").innerText;
-    document.getElementById("your-website").innerText = websiteLetter;
-    document.getElementById("website-letter").value = "";
+    inputText("logo-letter", "logo-part");
+    inputText("your-name", "name");
+    inputText("your-profession", "profession");
+    inputText("company-letter", "company-name");
+    inputText("tagline-letter","company-tagline");
+    inputText("your-email", "email");
+    inputText("your-contuct", "contuct");
+    inputText("location-letter", "your-location");
+    inputText("website-letter","your-website");
 })
